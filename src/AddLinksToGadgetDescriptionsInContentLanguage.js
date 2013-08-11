e74d5fa7c4c51c1cf4da4642982c94276e4c0afd
Helder <he7d3r@gmail.com>
2013-08-11 15:07:47 +0000

    Adds links to edit the gadget descriptions in site language to Special:Gadgets
    Used e.g. on https://pt.wikibooks.org/wiki/Special:Gadgets
    
    Note: this code was originally hosted on Portuguese Wikibooks (https://pt.wikibooks.org)

---
+++ src/AddLinksToGadgetDescriptionsInContentLanguage.js
@@ -0,0 +1,35 @@
+/**
+ * Adds links to edit the gadget descriptions in site language to [[Special:Gadgets]]
+ * @author: [[User:Helder.wiki]]
+ * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/AddLinksToGadgetDescriptionsInUserLanguage.js]] ([[File:User:Helder.wiki/Tools/AddLinksToGadgetDescriptionsInUserLanguage.js]])
+ */
+/*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
+/*global jQuery, mediaWiki */
+( function ( mw, $ ) {
+'use strict';
+
+function addLinks (){
+	var uLang = mw.config.get( 'wgUserLanguage' ),
+		reUrl = new RegExp( '(MediaWiki:Gadget-.+?)/' + uLang );
+	$( '#mw-content-text' ).find( 'a' ).filter( function(){
+		return reUrl.test( $( this ).attr( 'href' ) );
+	} ).each( function(){
+		var $this = $( this ),
+			$newLink = $this.clone().removeClass( 'new' ),
+			newTitle = $this.attr( 'title' ).match( reUrl );
+		if ( newTitle && newTitle[1] ){
+			$newLink.attr( {
+				title: newTitle[1],
+				href: $newLink.attr( 'href' ).replace( reUrl, newTitle[1] )
+			} ).append( ' [', mw.config.get( 'wgContentLanguage' ) ,']' );
+		}
+		$this.append( ' [', uLang , ']' )
+			.before( $newLink, ' | ' );
+	} );
+}
+
+if( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Gadgets' ){
+	$( addLinks );
+}
+
+}( mediaWiki, jQuery ) );
