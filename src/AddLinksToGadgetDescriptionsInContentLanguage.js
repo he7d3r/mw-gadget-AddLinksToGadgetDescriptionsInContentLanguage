2b7cebc0059b4fe54faf5ab4a7afaae478e8b278
Helder <he7d3r@gmail.com>
2014-09-27 20:48:54 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/AddLinksToGadgetDescriptionsInContentLanguage.js
+++ src/AddLinksToGadgetDescriptionsInContentLanguage.js
@@ -4,30 +4,30 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-function addLinks (){
-	var uLang = mw.config.get( 'wgUserLanguage' ),
-		reUrl = new RegExp( '(MediaWiki:Gadget-.+?)/' + uLang );
-	$( '#mw-content-text' ).find( 'a' ).filter( function(){
-		return reUrl.test( $( this ).attr( 'href' ) );
-	} ).each( function(){
-		var $this = $( this ),
-			$newLink = $this.clone().removeClass( 'new' ),
-			newTitle = $this.attr( 'title' ).match( reUrl );
-		if ( newTitle && newTitle[1] ){
-			$newLink.attr( {
-				title: newTitle[1],
-				href: $newLink.attr( 'href' ).replace( reUrl, newTitle[1] )
-			} ).append( ' [', mw.config.get( 'wgContentLanguage' ) ,']' );
-		}
-		$this.append( ' [', uLang , ']' )
-			.before( $newLink, ' | ' );
-	} );
-}
+	function addLinks() {
+		var uLang = mw.config.get( 'wgUserLanguage' ),
+			reUrl = new RegExp( '(MediaWiki:Gadget-.+?)/' + uLang );
+		$( '#mw-content-text' ).find( 'a' ).filter( function () {
+			return reUrl.test( $( this ).attr( 'href' ) );
+		} ).each( function () {
+			var $this = $( this ),
+				$newLink = $this.clone().removeClass( 'new' ),
+				newTitle = $this.attr( 'title' ).match( reUrl );
+			if ( newTitle && newTitle[1] ) {
+				$newLink.attr( {
+					title: newTitle[1],
+					href: $newLink.attr( 'href' ).replace( reUrl, newTitle[1] )
+				} ).append( ' [', mw.config.get( 'wgContentLanguage' ), ']' );
+			}
+			$this.append( ' [', uLang, ']' )
+				.before( $newLink, ' | ' );
+		} );
+	}
 
-if( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Gadgets' ){
-	$( addLinks );
-}
+	if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Gadgets' ) {
+		$( addLinks );
+	}
 
 }( mediaWiki, jQuery ) );
